efa45f1065d58613229616d8358c01d0d7d8d737
yangchao <chaoyue@yangchaodeMacBook-Pro.local>
2020-09-03 15:08:18 +0800

    flexible.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import 'lib-flexible'
+import '@/styles/reset.css'
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <div class="banner"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    // HelloWorld
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.banner{
+  width: 7.5rem;
+  height: 3.2rem;
+  border: 1px solid red;
+}
+</style>
